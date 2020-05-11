a76a03c0c1b485c36da2dc506aa6939c10a07538
rjro <rjro@me.com>
2020-05-10 17:53:46 -0700

    add note about sound

--- ChessGame/SoundPlayer.swift
+++ ChessGame/SoundPlayer.swift
@@ -15,11 +15,14 @@ class SoundPlayer {
 	
 	let bonk: AVAudioPlayer
 	
+	//This doesn't actually get called till the first time the singleton
+	//is made, so, it seeems like there's a slight lag in the
+	//sound being played the first time
+	//move this somewhere else!
 	init() {
 		let path = Bundle.main.path(forResource: "bonk", ofType: "wav")
 		let url = NSURL.fileURL(withPath: path!)
 		self.bonk = try! AVAudioPlayer(contentsOf: url)
-
 	}
 	
 	func playBonk() {
